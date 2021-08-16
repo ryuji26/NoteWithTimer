cded3bf8c842ee979a16f6aea72d96a5ddc760d8
ryuji <ryujispjp@gmail.com>
2021-08-16 13:06:27 +0900

    Initial Commit

---
+++ NoteWithTimer/NoteWithTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteWithTimerApp.swift
+//  NoteWithTimer
+//
+//  Created by 髙橋　竜治 on 2021/08/16.
+//
+
+import SwiftUI
+
+@main
+struct NoteWithTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
