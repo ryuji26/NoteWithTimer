89fae5bc89f7023f9ad290ea3add104a45ff1e23
ryuji <ryujispjp@gmail.com>
2021-08-16 16:22:12 +0900

    アラートの実装したらリリースできる

--- NoteWithTimer/TimerButtonView.swift
+++ NoteWithTimer/TimerButtonView.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import Combine
+import AudioToolbox
 
 /// タイマーポップオーバーを開くボタン
 struct TimerButton: View {
@@ -19,6 +20,22 @@ struct TimerButton: View {
             Text("\(timeManager.displayTimer())")   // カウントダウン表示
             Image(systemName: "timer")
             ButtonsView()
+                .onReceive(timeManager.timer) { _ in
+                    guard self.timeManager.timerStatus == .running else { return }
+                    //残り時間が0より大きい場合
+                    if self.timeManager.duration > 0 {
+                        //残り時間から -0.05 する
+                        self.timeManager.duration -= 0.05
+                        //残り時間が0以下の場合
+                    } else {
+                        //タイマーステータスを.stoppedに変更する
+                        self.timeManager.timerStatus = .stopped
+                        //アラーム音を鳴らす
+                        AudioServicesPlayAlertSoundWithCompletion(self.timeManager.soundID, nil)
+                        //バイブレーションを作動させる
+                        AudioServicesPlayAlertSoundWithCompletion(SystemSoundID(kSystemSoundID_Vibrate)) {}
+                    }
+                }
         }
         .font(Font.title.monospacedDigit())
         .onTapGesture { self.showTimerPopover = true }
